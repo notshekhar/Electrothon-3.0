784345803c7d74166e56305d7bc61d6742efcae7
Shekhar Tyagi <notshekhar@gmail.com>
2021-02-07 23:11:08 +0530

    new

--- public/js/bling.js
+++ public/js/bling.js
@@ -1,5 +1,20 @@
 window.$ = document.querySelector.bind(document)
 Element.prototype.on = Element.prototype.addEventListener
+function createElement(e, attrs) {
+    let el = document.createElement(e)
+    for (let attr in attrs) {
+        const value = attrs[attr]
+        if (attr == "innerText") el.innerText = value
+        else if (attr == "innerHTML") el.innerHTML = value
+        else el.setAttribute(attr, value)
+    }
+    return el
+}
+Element.prototype.css = function (styles) {
+    for (let style in styles) {
+        this.style[style] = styles[style]
+    }
+}
 
 //
 window._ = document.querySelectorAll.bind(document)
@@ -22,4 +37,7 @@ NodeList.prototype.css = function (styles) {
             el.style[style] = styles[style]
         }
     })
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+    this.forEach((el) => el.remove())
 }
